bf9193afb51de1fbdc15cfdf38af728b25f9b701
Arthur Milchior <Arthur@Milchior.fr>
2020-04-03 11:46:43 +0200

    .24

--- __init__.py
+++ __init__.py
@@ -1,14 +1,10 @@
-from aqt import gui_hooks
+from aqt import gui_hooks, mw
 from aqt.addcards import AddCards
 
 
-old_init = AddCards.__init__
-def init(add_cards, mw):
-    old_init(add_cards, mw)
+def init(add_cards):
     editor = add_cards.editor
     note = editor.note
-    if not isinstance(add_cards, AddCards):
-        return
     if note is None:
         return
     if mw.state != "review":
@@ -24,7 +20,4 @@ def init(add_cards, mw):
     current_deck = mw.col.decks.get(current_did)
     add_cards.deckChooser.setDeckName(current_deck["name"])
     
-# For .24:
-# gui_hooks.add_cards_did_init.append(init)
-
-AddCards.__init__ = init
+gui_hooks.add_cards_did_init.append(init)
